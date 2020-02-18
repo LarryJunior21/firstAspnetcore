7757271478fddd70de1e4baa8eab492492d8d24a
unknown <larryjunior21@hotmail.com>
2020-02-18 20:25:28 -0300

    first commit

---
+++ HelloWorld/HelloWorld/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logar(string usuario, string senha)
+        { 
+            ViewBag.Usuario = usuario;
+            ViewBag.Senha = senha;
+
+            //ViewBag.Usuario = Request.Form["usuario"];
+            //ViewBag.Senha= Request.Form["senha"];
+
+            ViewBag.Msg = "Dados inválidos";
+            return View("Index");
+        }
+    }
+}
